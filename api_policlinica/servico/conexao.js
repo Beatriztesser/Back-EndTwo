27e22d3beb87bd83fae8edee3234f450cc1a1e55
Beatriztesser <beatrizeduardabt@gmail>
2025-02-03 11:28:25 -0400

    aula fs 03/02

--- api_policlinica/servico/conexao.js
+++ api_policlinica/servico/conexao.js
@@ -6,7 +6,7 @@ import mysql from 'mysql2/promise'
 const pool = mysql.createPool({
     host:'localhost',
     user: 'root',
-    password: 'nova_senha',
+    password: 'Suporte99',
     database: 'clinica'
 })
 export default pool;
